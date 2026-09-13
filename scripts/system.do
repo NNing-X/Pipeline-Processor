c93a34e810db1f75cc653e90917615b781897b89
Hening Xu <xu2005@purdue.edu>
2026-09-13 13:50:28 -0400

    success system sim

---
+++ scripts/system.do
@@ -0,0 +1,32 @@
+onerror {resume}
+quietly WaveActivateNextPane {} 0
+add wave -noupdate /system_tb/CLK
+add wave -noupdate /system_tb/nRST
+add wave -noupdate /system_tb/dbg_data_out_tb
+add wave -noupdate /system_tb/dbg_addr_tb
+add wave -noupdate -divider syif
+add wave -noupdate /system_tb/syif/tbCTRL
+add wave -noupdate /system_tb/syif/halt
+add wave -noupdate /system_tb/syif/WEN
+add wave -noupdate /system_tb/syif/REN
+add wave -noupdate /system_tb/syif/addr
+add wave -noupdate /system_tb/syif/store
+add wave -noupdate /system_tb/syif/load
+TreeUpdate [SetDefaultTree]
+WaveRestoreCursors {{Cursor 1} {2191101559 ps} 0}
+quietly wave cursor active 1
+configure wave -namecolwidth 150
+configure wave -valuecolwidth 100
+configure wave -justifyvalue left
+configure wave -signalnamewidth 1
+configure wave -snapdistance 10
+configure wave -datasetprefix 0
+configure wave -rowmargin 4
+configure wave -childrowmargin 2
+configure wave -gridoffset 0
+configure wave -gridperiod 1
+configure wave -griddelta 40
+configure wave -timeline 0
+configure wave -timelineunits ns
+update
+WaveRestoreZoom {5735450 ps} {2414624450 ps}
--- source/datapath.sv
+++ source/datapath.sv
@@ -22,12 +22,24 @@ module datapath (
   // pc init
   parameter PC_INIT = 0;
 
-  register_file reg0 (.CLK, .nRST, );
-  alu alu0 ()
+  //interface
+  register_file_if rfif();
+  alu_if aluif();
+
+  register_file RF0 (CLK, nRST, rfif);
+  alu ALU0 (aluif);
+
+  assign rfif.rsel1 = dpif.imemload[19:15];
+  assign rfif.rsel2 = dpif.imemload[24:20];
+  assign rfif.wsel = dpif.imemload[11:7];
+  assign aluif.portA = rfif.rdat1;
+
+  assign dpif.dmemstore = rfif.rdat2;
+  assign dpif.dmemaddr = aluif.outputPort;
 
   //decoder
-  logic is_halt, beq, bne, blt, bge, Jal, Jalr, ALUSrc, MemRead, MemWrite, Regwrite, auipc, lui;
-  logic [4:0] ALUOp;
+  logic is_halt, beq, bne, blt, bge, Jal, Jalr, ALUSrc, MemRead, MemWrite, RegWrite, auipc, lui;
+  // logic [4:0] ALUOp;
   logic [2:0] funct3;
   logic [6:0] funct7;
   logic [6:0] opcode;
@@ -41,42 +53,41 @@ module datapath (
         Jalr = 1'b1;
         ALUSrc = 1'b1;
         RegWrite = 1'b1;
-        ALUOp = ALU_ADD;
+        aluif.op = ALU_ADD;
       end
       7'b1101111: begin //jal
         Jal = 1'b1;
         ALUSrc = 1'b1;
         RegWrite = 1'b1;
-        ALUOp = ALU_ADD;
+        aluif.op = ALU_ADD;
       end
       7'b1111111: begin //halt
         is_halt = 1'b1;
       end
       7'b1100011: begin //branch type
-        Branch = 1'b1;
         ALUSrc = 1'b1;
         if (funct3 == 3'h0) begin
-          ALUOp = ALU_SUB;
+          aluif.op = ALU_SUB;
           beq = 1'b1;
         end
         if (funct3 == 3'h1) begin
-          ALUOp = ALU_SUB;
+          aluif.op = ALU_SUB;
           bne = 1'b1;
         end
         if (funct3 == 3'h4) begin
-          ALUOp = ALU_SLT;
+          aluif.op = ALU_SLT;
           blt = 1'b1;
         end
         if (funct3 == 3'h5) begin
-          ALUOp = ALU_SLT;
+          aluif.op = ALU_SLT;
           bge = 1'b1;
         end
         if (funct3 == 3'h6) begin
-          ALUOp = ALU_SLTU;
+          aluif.op = ALU_SLTU;
           blt = 1'b1;
         end
         if (funct3 == 3'h7) begin
-          ALUOp = ALU_SLTU;
+          aluif.op = ALU_SLTU;
           bge = 1'b1;
         end
       end
@@ -93,29 +104,29 @@ module datapath (
       7'b0010011: begin //i type except load, eg.addi,xori,ori,andi,slli
         ALUSrc = 1'b1;
         RegWrite = 1'b1;
-        if (funct3 == 3'h0) ALUOp = ALU_ADD;
-        if (funct3 == 3'h4) ALUOp = ALU_XOR;
-        if (funct3 == 3'h6) ALUOp = ALU_OR;
-        if (funct3 == 3'h7) ALUOp = ALU_AND;
-        if (funct3 == 3'h1 && funct7 == 7'h00) ALUOp = ALU_SLL;
-        if (funct3 == 3'h5 && funct7 == 7'h00) ALUOp = ALU_SRL;
-        if (funct3 == 3'h5 && funct7 == 7'h20) ALUOp = ALU_SRA;
-        if (funct3 == 3'h2) ALUOp = ALU_SLT;
-        if (funct3 == 3'h3) ALUOp = ALU_SLTU;
+        if (funct3 == 3'h0) aluif.op = ALU_ADD;
+        if (funct3 == 3'h4) aluif.op = ALU_XOR;
+        if (funct3 == 3'h6) aluif.op = ALU_OR;
+        if (funct3 == 3'h7) aluif.op = ALU_AND;
+        if (funct3 == 3'h1 && funct7 == 7'h00) aluif.op = ALU_SLL;
+        if (funct3 == 3'h5 && funct7 == 7'h00) aluif.op = ALU_SRL;
+        if (funct3 == 3'h5 && funct7 == 7'h20) aluif.op = ALU_SRA;
+        if (funct3 == 3'h2) aluif.op = ALU_SLT;
+        if (funct3 == 3'h3) aluif.op = ALU_SLTU;
       end
       7'b0110011: begin //register type, eg.add,sub,xor...
         ALUSrc = 1'b0;
         RegWrite = 1'b1;
-        if (funct3 == 3'h0 && funct7 == 7'h00) ALUOp = ALU_ADD;
-        if (funct3 == 3'h0 && funct7 == 7'h20) ALUOp = ALU_SUB;
-        if (funct3 == 3'h4 && funct7 == 7'h00) ALUOp = ALU_XOR;
-        if (funct3 == 3'h6 && funct7 == 7'h00) ALUOp = ALU_OR;
-        if (funct3 == 3'h7 && funct7 == 7'h00) ALUOp = ALU_AND;
-        if (funct3 == 3'h1 && funct7 == 7'h00) ALUOp = ALU_SLL;
-        if (funct3 == 3'h5 && funct7 == 7'h00) ALUOp = ALU_SRL;
-        if (funct3 == 3'h5 && funct7 == 7'h20) ALUOp = ALU_SRA;
-        if (funct3 == 3'h2 && funct7 == 7'h00) ALUOp = ALU_SLT;
-        if (funct3 == 3'h3 && funct7 == 7'h00) ALUOp = ALU_SLTU;
+        if (funct3 == 3'h0 && funct7 == 7'h00) aluif.op = ALU_ADD;
+        if (funct3 == 3'h0 && funct7 == 7'h20) aluif.op = ALU_SUB;
+        if (funct3 == 3'h4 && funct7 == 7'h00) aluif.op = ALU_XOR;
+        if (funct3 == 3'h6 && funct7 == 7'h00) aluif.op = ALU_OR;
+        if (funct3 == 3'h7 && funct7 == 7'h00) aluif.op = ALU_AND;
+        if (funct3 == 3'h1 && funct7 == 7'h00) aluif.op = ALU_SLL;
+        if (funct3 == 3'h5 && funct7 == 7'h00) aluif.op = ALU_SRL;
+        if (funct3 == 3'h5 && funct7 == 7'h20) aluif.op = ALU_SRA;
+        if (funct3 == 3'h2 && funct7 == 7'h00) aluif.op = ALU_SLT;
+        if (funct3 == 3'h3 && funct7 == 7'h00) aluif.op = ALU_SLTU;
       end
       7'b0010111: begin //auipc
         RegWrite = 1'b1;
@@ -135,9 +146,9 @@ module datapath (
         Jalr = '0;
         ALUSrc = '0;
         MemRead = '0;
-        MemWrtie = '0;
+        MemWrite = '0;
         RegWrite = '0;
-        ALUOp = '0;
+        aluif.op = '0;
         auipc = '0;
         lui = '0;
       end
@@ -153,9 +164,9 @@ module datapath (
         if (funct3 == 3'h4) imm = {{20{dpif.imemload[31]}},dpif.imemload[31:20]};
         if (funct3 == 3'h6) imm = {{20{dpif.imemload[31]}},dpif.imemload[31:20]};
         if (funct3 == 3'h7) imm = {{20{dpif.imemload[31]}},dpif.imemload[31:20]};
-        if (funct3 == 3'h1 && funct7 == 7'h00) {{27{dpif.imemload[31]}},dpif.imemload[24:20]};
-        if (funct3 == 3'h5 && funct7 == 7'h00) {{27{dpif.imemload[31]}},dpif.imemload[24:20]};
-        if (funct3 == 3'h5 && funct7 == 7'h20) {{27{dpif.imemload[31]}},dpif.imemload[24:20]};
+        if (funct3 == 3'h1 && funct7 == 7'h00) imm = {{27{dpif.imemload[31]}},dpif.imemload[24:20]};
+        if (funct3 == 3'h5 && funct7 == 7'h00) imm = {{27{dpif.imemload[31]}},dpif.imemload[24:20]};
+        if (funct3 == 3'h5 && funct7 == 7'h20) imm = {{27{dpif.imemload[31]}},dpif.imemload[24:20]};
         if (funct3 == 3'h2) imm = {{20{dpif.imemload[31]}},dpif.imemload[31:20]};
         if (funct3 == 3'h3) imm = {20'h0_0000,dpif.imemload[31:20]};
       end
@@ -173,16 +184,16 @@ module datapath (
           imm = {{19'h0_0000},dpif.imemload[31],dpif.imemload[7],dpif.imemload[30:25],dpif.imemload[11:8],1'b0};
         end
       end
-      default: imm = '0
+      default: imm = '0;
     endcase
   end
 
   //ALUSrc mux
   always_comb begin
     casez(ALUSrc)
-      1'b0: portB = rdat2;
-      1'b1: portB = imm;
-      default: portB = '0;
+      1'b0: aluif.portB = rfif.rdat2;
+      1'b1: aluif.portB = imm;
+      default: aluif.portB = '0;
     endcase
   end
 
@@ -208,16 +219,16 @@ module datapath (
   end
   //output logic
   always_comb begin
-    halt = 1'b0;
-    imemREN = 1'b1;
+    dpif.halt = 1'b0;
+    dpif.imemREN = 1'b1;
     casez(state)
       IDLE: begin
-        halt = 1'b0;
-        imemREN = 1'b1;
+        dpif.halt = 1'b0;
+        dpif.imemREN = 1'b1;
       end
       HALT: begin
-        halt = 1'b1;
-        imemREN = 1'b0;
+        dpif.halt = 1'b1;
+        dpif.imemREN = 1'b0;
       end
     endcase
   end
@@ -230,7 +241,7 @@ module datapath (
     end
   end
   //request unit
-  logic next_instr_done, instr_done;
+  logic next_instr_done, instr_done, next_dmemREN, next_dmemWEN, next_WEN;
   always_comb begin
     if (dpif.ihit) begin
       if (MemRead) begin
@@ -241,10 +252,12 @@ module datapath (
       end
       else begin
         next_instr_done = 1'b1;
+        next_WEN = 1'b1;
       end
     end
     if (dpif.dhit) begin
       next_instr_done = 1'b1;
+      next_WEN = 1'b1;
       next_dmemREN = 1'b0;
       next_dmemWEN = 1'b0;
     end
@@ -252,11 +265,13 @@ module datapath (
   always_ff @(posedge CLK, negedge nRST) begin
     if (~nRST) begin
       instr_done <= '0;
+      rfif.WEN <= '0;
       dpif.dmemREN <= '0;
       dpif.dmemWEN <= '0;
     end
     else begin
       instr_done <= next_instr_done;
+      rfif.WEN <= next_WEN;
       dpif.dmemREN <= next_dmemREN;
       dpif.dmemWEN <= next_dmemWEN;
     end
@@ -266,20 +281,10 @@ module datapath (
   logic [1:0] PCSrc;
   always_comb begin
     PCSrc = 2'b00;
-    if (beq && zero || bne && ~zero || blt && outputPort || bge && ~outputPort || Jal) begin
+    if (beq && aluif.zero || bne && ~aluif.zero || blt && aluif.outputPort || bge && ~aluif.outputPort || Jal) begin
       PCSrc = 2'b01;
     end
     else if (Jalr) PCSrc = 2'b10;
-  end
-
-  //PCSrc mux
-  always_comb begin
-    casez(PCSrc)
-      2'b00: next_imemaddr = pcPlus4;
-      2'b01: next_imemaddr = imm_pc;
-      2'b10: next_imemaddr = outputPort;
-      default: next_imemaddr = PC_INIT;
-    endcase
   end
 
   //pc logic
@@ -289,33 +294,44 @@ module datapath (
     imm_pc = dpif.imemaddr + imm;
   end
 
+  //PCSrc mux
+  logic [31:0] next_imemaddr;
+  always_comb begin
+    casez(PCSrc)
+      2'b00: next_imemaddr = pcPlus4;
+      2'b01: next_imemaddr = imm_pc;
+      2'b10: next_imemaddr = aluif.outputPort;
+      default: next_imemaddr = PC_INIT;
+    endcase
+  end
+
   //pc register
   always_ff @(posedge CLK, negedge nRST)begin
     if (~nRST) begin
       dpif.imemaddr = PC_INIT;
     end
     else if (instr_done) begin
-      dpif.imemaddr = next_imemaddr
+      dpif.imemaddr = next_imemaddr;
     end
   end
 
   //wdat_control logic
   logic [2:0] MemtoReg;
   always_comb begin
-    MemtoReg = 3'b000
+    MemtoReg = 3'b000;
     if (auipc) MemtoReg = 3'b001;
-    if (jal || jalr) MemtoReg = 3'b010;
+    if (Jal || Jalr) MemtoReg = 3'b010;
     if (MemRead) MemtoReg = 3'b011;
     if (lui) MemtoReg = 3'b100;
   end
   //wdat mux
   always_comb begin
     casez(MemtoReg)
-      3'b000: wdat = outputPort;
-      3'b001: wdat = imm_pc;
-      3'b010: wdat = pcPlus4;
-      3'b011: wdat = dmemload;
-      3'b100: wdta = imm;
+      3'b000: rfif.wdat = aluif.outputPort;
+      3'b001: rfif.wdat = imm_pc;
+      3'b010: rfif.wdat = pcPlus4;
+      3'b011: rfif.wdat = dpif.dmemload;
+      3'b100: rfif.wdat = imm;
     endcase
   end
 
